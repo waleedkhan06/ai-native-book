e15a44c54aad7973577e1743329174ddcef3460e
waleedkhan06 <waleedcrucial2006@gmail.com>
2025-12-23 20:58:23 +0500

    deploybook

--- my-web/docusaurus.config.ts
+++ my-web/docusaurus.config.ts
@@ -12,20 +12,20 @@ const config: Config = {
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
 
+  },//
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ai-native-book-3dpxt92tc-waleed0248s-projects.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
+  //  GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'haclathon', // Usually your GitHub org/user name.
-  projectName: 'ai-native-book', // Usually your repo name.
+  // organizationName: 'haclathon', // Usually your GitHub org/user name.
+  // projectName: 'ai-native-book', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  // onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
